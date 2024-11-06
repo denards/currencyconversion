a96b001341dc8519e106cd66cc4d6a02c2b8fa81
Denard Soares <denard.soares.ext@sascar.com.br>
2024-11-06 00:18:42 -0300

    Criação do repository, do DTO, da exceção de objeto não encontrado, e separação do ExchangeRateResponse

--- src/main/java/com/sognisport/currencyconversion/controller/ExchangeRateController.java
+++ src/main/java/com/sognisport/currencyconversion/controller/ExchangeRateController.java
@@ -1,16 +1,14 @@
 package com.sognisport.currencyconversion.controller;
 
 import com.sognisport.currencyconversion.domain.dto.ConversionRate;
+import com.sognisport.currencyconversion.domain.dto.ConversionRateDTO;
 import com.sognisport.currencyconversion.service.ExchangRateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/exchange")
@@ -19,11 +17,21 @@ public class ExchangeRateController {
     @Autowired
     ExchangRateService exchangRateService;
 
-    @GetMapping(path = "/rate/{from}/{to}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/rate/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ConversionRate> getExchangeRate(@PathVariable String from, @PathVariable String to) {
         System.out.println(new StringBuilder("variaveis: ").append(from).append(" ").append(to));
 
-        return ResponseEntity.ok(exchangRateService.getExchangeRate(from,to));
+        return ResponseEntity.ok(exchangRateService.getExchangeRate(from, to));
+    }
+
+    @GetMapping("/listexchangerates")
+    public List<ConversionRate> listExchangeRates() {
+        return exchangRateService.listAllExcnahgeRates();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ConversionRate> updateConversionRate(@RequestBody ConversionRateDTO conversionRateDTO) {
+        return ResponseEntity.ok(exchangRateService.updateConversionRate(conversionRateDTO));
     }
 }
 
--- src/main/java/com/sognisport/currencyconversion/service/ExchangRateService.java
+++ src/main/java/com/sognisport/currencyconversion/service/ExchangRateService.java
@@ -1,12 +1,18 @@
 package com.sognisport.currencyconversion.service;
 
 import com.sognisport.currencyconversion.domain.dto.ConversionRate;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.sognisport.currencyconversion.domain.dto.ConversionRateDTO;
+import com.sognisport.currencyconversion.domain.entity.ExchangeRateResponse;
+import com.sognisport.currencyconversion.exception.ObjectNotFoundException;
+import com.sognisport.currencyconversion.repository.CurrencyConversionRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class ExchangRateService {
@@ -15,37 +21,44 @@ public class ExchangRateService {
     //analisar uma troca por WebClient
 
     private final RestTemplate restTemplate;
+    private final CurrencyConversionRepository currencyConversionRepository;
 
     @Value("${com.sognisport.exchange.apikey}")
     private String apiKey;
 
-    public ExchangRateService(RestTemplate restTemplate){
+    public ExchangRateService(RestTemplate restTemplate, CurrencyConversionRepository currencyConversionRepository) {
         this.restTemplate = restTemplate;
+        this.currencyConversionRepository = currencyConversionRepository;
     }
 
-    public ConversionRate getExchangeRate(String fromCurrency, String toCurrency){
+    public ConversionRate getExchangeRate(String fromCurrency, String toCurrency) {
 
-        String url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/%s/%s",apiKey, fromCurrency, toCurrency);
+        String url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/%s/%s", apiKey, fromCurrency, toCurrency);
         ExchangeRateResponse exchangeRateResponse = restTemplate.getForObject(url, ExchangeRateResponse.class);
-        ConversionRate conversionRate = new ConversionRate();
-        conversionRate.setFromCurrency(fromCurrency);
-        conversionRate.setToCurrency(toCurrency);
-        conversionRate.setRate(exchangeRateResponse.getConversion_rate());
-        conversionRate.setTimestamp(LocalDateTime.now());
+        ConversionRate conversionRate = null;
+        if (!Objects.isNull(exchangeRateResponse)) {
+            conversionRate = new ConversionRate();
+            conversionRate.setFromCurrency(fromCurrency);
+            conversionRate.setToCurrency(toCurrency);
+            conversionRate.setRate(exchangeRateResponse.getConversion_rate());
+            conversionRate.setTimestamp(LocalDateTime.now());
+        }
 
-        return conversionRate;
+        return currencyConversionRepository.save(conversionRate);
     }
 
-    private static class ExchangeRateResponse {
-        private Double conversion_rate;
 
+    public List<ConversionRate> listAllExcnahgeRates() {
+        return currencyConversionRepository.findAll();
+    }
 
-        public Double getConversion_rate() {
-            return conversion_rate;
-        }
+    public ConversionRate updateConversionRate(ConversionRateDTO conversionRateDTO) {
+        Optional<ConversionRate> op = currencyConversionRepository.findById(conversionRateDTO.getId());
+        op.ifPresentOrElse(
+                (conversionRate -> conversionRate.setRate(conversionRateDTO.getRate())),
+                () -> new ObjectNotFoundException("object " + conversionRateDTO.getId() + " not found")
+        );
 
-        public void setConversion_rate(Double conversion_rate) {
-            this.conversion_rate = conversion_rate;
-        }
+        return op.get();
     }
 }
